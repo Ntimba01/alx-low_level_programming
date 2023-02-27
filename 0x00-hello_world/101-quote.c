9454147e36454fe46f6252f3835c5b0a551cbd17
tania <ntima@gmail.com>
2023-02-27 15:25:10 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 include <unistd.h>
 /**
- * main - A c program that print a line to the standard error
- * return 1 (succecc)
+ * main - Entry point
+ *
+ * Return: 1 (succecc)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
