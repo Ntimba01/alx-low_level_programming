ee6d3dd4799f4e7fbfd71078b0068823e05bf363
tania <ntima@gmail.com>
2023-02-28 14:34:20 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number ot the times table
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
@@ -28,22 +28,20 @@ void print_times_table(int n)
 				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-                                        _putchar(' ');
-                                        _putchar(' ');
-                                        _putchar((k / 10) + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
 				{
 					_putchar(',');
-                                        _putchar(' ');
-                                        _putchar((k / 100) + '0');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
-                                        _putchar((k % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
 		}
 	}
 }
-
-
