df4c32faa1eba7da4e396dee4c9b8d5db7a32f96
tania <ntima@gmail.com>
2023-02-28 14:09:02 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,6 +3,7 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
+
 void times_table(void)
 {
 	int i, j, k;
@@ -17,11 +18,12 @@ void times_table(void)
 				_putchar(k + '0');
 			}
 
-			if  (k < 10 && j != 0)
+			if (k < 10 && j != 0)
+
 			{
 				 _putchar(',');
 				 _putchar(' ');
-				 _putchar(',');
+				 _putchar(' ');
 				 _putchar(k + '0');
 			} else if (k >= 10)
 			{
