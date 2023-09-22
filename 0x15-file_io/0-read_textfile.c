84c0215379f9fec37a258d13c6c1f4f9cef0f727
ntimba01 <ntimba01@gmail.com>
2023-09-22 06:12:44 -0700

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,21 +1,24 @@
 #include "main.h"
-#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+
 /**
- * read_textfile - files print to STDOUT read and write
- * @filename:read text file using the file name
- * @letters: number of letters is to be read
- * Return: Clean up and return the number of bytes read and written
- * when function fails or filename is NULL it must be 0
- * return to 0 always
+ * read_textfile - reads a text file and prints standard output
+ * @filename: name of the file text is being  read
+ * @letters: number of letters it should read and print file
+ *
+ * Return: actual number of letters it could read and print always
+ * 0 when function fails or filename is NULL.
  */
 
-ssize_t read_textfile(const char, filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd;
 	char *buf;
-	ssize_t t, w;
+	ssize_t redt, redw;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -26,15 +29,15 @@ ssize_t read_textfile(const char, filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-	w = write(STDOUT_FILENO, buf, t);
-	if (w == -1)
+	redw = write(STDOUT_FILENO, buf, redt);
+	if (redw == -1)
 	{
 		close(fd);
 		free(buf);
 		return (0);
 	}
-	t = read(buf, fd, letters);
-	if (t == -1)
+	redt = read(buf, fd, letters);
+	if (redt == -1)
 	{
 		close(fd);
 		free(buf);
@@ -42,5 +45,5 @@ ssize_t read_textfile(const char, filename, size_t letters)
 	}
 	close(fd);
 	free(buf);
-	return (w);
+	return (redw);
 }
