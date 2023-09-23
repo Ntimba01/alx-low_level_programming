b3c7fc3771b62d01c2d508255d18a9adf98740c3
ntimba01 <ntimba01@gmail.com>
2023-09-23 07:28:35 -0700

    test

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * get_bit - retrieve value of bit at a given index from unsigned long integer (n)
+ * get_bit - retrieve bit at a given index from unsigned long integer(n)
  * @index: greater than 63 (which is the maximum index for a 64-bit integer)
  *
  * Return: bit_val, which is either 0 or 1, or -1 in case of an invalid index.
