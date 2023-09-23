76bf7e93fb34665e6be1e29baaee4a3734b7c9b3
ntimba01 <ntimba01@gmail.com>
2023-09-23 07:21:04 -0700

    test

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * clear_bit- clear (change to 0) bit at index in an unsigned long integer (*n).
+ * clear_bit- clear bit at index in an unsigned long integer (*n).
  * @n: pointer to the unsigned long integer to be modified,
  * @index:  position of the bit to clear.
  *
